e15416d6916549d00c8430e9f99fd1bb8081c583
Stefano Gallotti <sgallotti@expedia.com>
2019-08-17 17:04:41 -0700

    introduce await keyword (syncronous)

--- src/sync_io_main.py
+++ src/sync_io_main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import asyncio
 
 
-def very_long_routine(index):
+async def very_long_routine(index):
     print(f"    start: {index}")
     time.sleep(5)
     print(f"    end:   {index}")
@@ -16,7 +16,7 @@ async def main():
     print(f"started at {program_start_time}")
     for i in range(1, 6):
         print(f"    iteration {i} started at {datetime.utcnow()}")
-        very_long_routine(i)
+        await very_long_routine(i)
         print(f"    iteration {i} ended   at {datetime.utcnow()}")
     program_end_time = datetime.utcnow()
     print(f"ended   at {program_end_time}")
@@ -24,5 +24,6 @@ async def main():
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    LOOP = asyncio.get_event_loop()
+    LOOP.run_until_complete(main())
+    LOOP.close()
