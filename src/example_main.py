01447804b0fa341c20d3d4989889a8106cfbe8c3
Stefano Gallotti <sgallotti@expedia.com>
2019-08-18 07:34:40 -0700

    introduce await (still syncronous)

--- src/example_main.py
+++ src/example_main.py
@@ -1,13 +1,14 @@
 """
-    still syncronous as before, but this time using asyncio
+    using asyncio and await, but still syncronous as using a blocking function
 """
 import time
 from datetime import datetime
 import asyncio
 
-
-def very_long_routine(index):
+# since very_long_routine is awaited in main(), it has to be an async function
+async def very_long_routine(index):
     print(f"    start: {index}")
+    # note that time.sleep() is a blocking function. Prevents await from working
     time.sleep(5)
     print(f"    end:   {index}")
 
@@ -18,7 +19,8 @@ async def main():
     print(f"started at {program_start_time}")
     for i in range(1, 6):
         print(f"    iteration {i} started at {datetime.utcnow()}")
-        very_long_routine(i)
+        # invokes very_long_routine in asyncronous mode
+        await very_long_routine(i)
         print(f"    iteration {i} ended   at {datetime.utcnow()}")
     program_end_time = datetime.utcnow()
     print(f"ended   at {program_end_time}")
